5163f06f1db9faa554a46ba6601ce9b8b1cbcd9e
ikesau <saunders.ike@gmail.com>
2020-06-13 12:01:07 -0400

    ProgressBar

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,5 @@
 import { FunctionalComponent, h } from 'preact'
+import { useState } from 'preact/hooks'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 if ((module as any).hot) {
@@ -6,8 +7,45 @@ if ((module as any).hot) {
     require('preact/debug')
 }
 
+interface Step {
+    component: FunctionalComponent
+}
+
+const steps: Step[] = [
+    {
+        component: () => <div>step 1</div>,
+    },
+    {
+        component: () => <div>step 2</div>,
+    },
+    {
+        component: () => <div>step 3</div>,
+    },
+]
+
+const ProgressBar: FunctionalComponent<{ percentage: number }> = ({
+    percentage,
+}) => (
+    <div className='progress-bar'>
+        <div
+            className='progress-bar__fill'
+            style={{ width: `${percentage}%` }}
+        ></div>
+    </div>
+)
+
 const App: FunctionalComponent = () => {
-    return <div id='app'>hello</div>
+    const [activeStep, setActiveStep] = useState(0)
+    const Step = steps[activeStep].component
+    console.log('Step', Step)
+    return (
+        <div id='app'>
+            <ProgressBar percentage={(activeStep / (steps.length - 1)) * 100} />
+            <h1>Give Simple</h1>
+            {/* <Step /> */}
+            <button onClick={() => setActiveStep(activeStep + 1)}>Next</button>
+        </div>
+    )
 }
 
 export default App
