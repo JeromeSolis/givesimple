6c42953a026e9c0068e9607400b83413a0bd3307
Jerome Solis <solis.jerome@gmail.com>
2020-06-24 23:33:42 -0400

    Completed translation of Python code to Typescript

---
+++ models.js
@@ -0,0 +1,97 @@
+var giving_what_we_can_pledge = 0.1;
+var donation_limit = 200;
+var high_income_tax_credit_rate = 0.33;
+var high_income_limit = 200000;
+var canada_charity_tax_credit_data = [
+    {
+        region: 'CA',
+        rate_sub_200: 0.15,
+        rate_over_200: 0.29
+    },
+    {
+        region: 'AB',
+        rate_sub_200: 0.10,
+        rate_over_200: 0.21
+    },
+    {
+        region: 'BC',
+        rate_sub_200: 0.05,
+        rate_over_200: 0.15
+    },
+    {
+        region: 'MB',
+        rate_sub_200: 0.11,
+        rate_over_200: 0.17
+    },
+    {
+        region: 'NB',
+        rate_sub_200: 0.10,
+        rate_over_200: 0.18
+    },
+    {
+        region: 'NL',
+        rate_sub_200: 0.09,
+        rate_over_200: 0.18
+    },
+    {
+        region: 'NS',
+        rate_sub_200: 0.09,
+        rate_over_200: 0.21
+    },
+    {
+        region: 'NT',
+        rate_sub_200: 0.06,
+        rate_over_200: 0.14
+    },
+    {
+        region: 'NU',
+        rate_sub_200: 0.04,
+        rate_over_200: 0.12
+    },
+    {
+        region: 'ON',
+        rate_sub_200: 0.05,
+        rate_over_200: 0.11
+    },
+    {
+        region: 'PE',
+        rate_sub_200: 0.10,
+        rate_over_200: 0.17
+    },
+    {
+        region: 'QC',
+        rate_sub_200: 0.20,
+        rate_over_200: 0.24
+    },
+    {
+        region: 'SK',
+        rate_sub_200: 0.11,
+        rate_over_200: 0.15
+    },
+    {
+        region: 'YT',
+        rate_sub_200: 0.06,
+        rate_over_200: 0.13
+    }
+];
+function getTaxCredit(donationAmount, yearlyIncome, highIncome, region) {
+    var federalData = canada_charity_tax_credit_data.find(function (data) { return data.region === 'CA'; });
+    var provincialData = canada_charity_tax_credit_data.find(function (data) { return data.region === region; });
+    var federalCredit = 0;
+    var provincialCredit = 0;
+    if (highIncome && donationAmount > donation_limit) {
+        federalCredit = (donation_limit * federalData.rate_sub_200) + Math.min(yearlyIncome - high_income_limit, donationAmount - donation_limit) * high_income_tax_credit_rate + ((donationAmount - donation_limit) - Math.min(yearlyIncome - high_income_limit, donationAmount - donation_limit)) * federalData.rate_over_200;
+        provincialCredit = (donation_limit * provincialData.rate_sub_200) + (donationAmount - donation_limit) * provincialData.rate_over_200;
+    }
+    else if (!highIncome && donationAmount > donation_limit) {
+        federalCredit = (donation_limit * federalData.rate_sub_200) + (donationAmount - donation_limit) * federalData.rate_over_200;
+        provincialCredit = (donation_limit * provincialData.rate_sub_200) + (donationAmount - donation_limit) * provincialData.rate_over_200;
+    }
+    else {
+        federalCredit = donationAmount * federalData.rate_sub_200;
+        provincialCredit = donationAmount * provincialData.rate_sub_200;
+    }
+    return [federalCredit, provincialCredit];
+}
+var value = getTaxCredit(20000, 215000, true, 'QC');
+console.log(value[0] + value[1]);
--- models.ts
+++ models.ts
@@ -1,52 +1,103 @@
 const giving_what_we_can_pledge = 0.1
 const donation_limit = 200
 const high_income_tax_credit_rate = 0.33
+const high_income_limit = 200000
 
-/*
-Header: region, 2017 rates <=200, 2017 rates >200, 2016 rates <=200, 2016 rates >200
-*/
-const canada_charity_tax_credit_data: [string, number, number, number, number][] =
-    [['CA', 0.15, 0.29, 0.15, 0.29],
-    ['AB', 0.10, 0.21, 0.10, 0.21],
-    ['BC', 0.05, 0.15, 0.05, 0.15],
-    ['MB', 0.11, 0.17, 0.11, 0.17],
-    ['NB', 0.10, 0.18, 0.10, 0.18],
-    ['NL', 0.09, 0.18, 0.08, 0.17],
-    ['NS', 0.09, 0.21, 0.09, 0.21],
-    ['NT', 0.06, 0.14, 0.06, 0.14],
-    ['NU', 0.04, 0.12, 0.04, 0.12],
-    ['ON', 0.05, 0.11, 0.05, 0.11],
-    ['PE', 0.10, 0.17, 0.10, 0.17],
-    ['QC', 0.20, 0.24, 0.20, 0.24],
-    ['SK', 0.11, 0.15, 0.11, 0.15],
-    ['YT', 0.06, 0.13, 0.06, 0.13]];
-
-function getData(region: string) {
-  let data: [number, number, number, number]
-  for (let index in canada_charity_tax_credit_data) {
-    if (canada_charity_tax_credit_datap[index][0] == region)
-      canada_charity_tax_credit_data.forEach(element => {
-
-      });[index]
+var canada_charity_tax_credit_data = [
+  {
+    region: 'CA',
+    rate_sub_200: 0.15,
+    rate_over_200: 0.29,
+  },
+  {
+    region: 'AB',
+    rate_sub_200: 0.10,
+    rate_over_200: 0.21,
+  },
+  {
+    region: 'BC',
+    rate_sub_200: 0.05,
+    rate_over_200: 0.15,
+  },
+  {
+    region: 'MB',
+    rate_sub_200: 0.11,
+    rate_over_200: 0.17,
+  },
+  {
+    region: 'NB',
+    rate_sub_200: 0.10,
+    rate_over_200: 0.18,
+  },
+  {
+    region: 'NL',
+    rate_sub_200: 0.09,
+    rate_over_200: 0.18,
+  },
+  {
+    region: 'NS',
+    rate_sub_200: 0.09,
+    rate_over_200: 0.21,
+  },
+  {
+    region: 'NT',
+    rate_sub_200: 0.06,
+    rate_over_200: 0.14,
+  },
+  {
+    region: 'NU',
+    rate_sub_200: 0.04,
+    rate_over_200: 0.12,
+  },
+  {
+    region: 'ON',
+    rate_sub_200: 0.05,
+    rate_over_200: 0.11,
+  },
+  {
+    region: 'PE',
+    rate_sub_200: 0.10,
+    rate_over_200: 0.17,
+  },
+  {
+    region: 'QC',
+    rate_sub_200: 0.20,
+    rate_over_200: 0.24,
+  },
+  {
+    region: 'SK',
+    rate_sub_200: 0.11,
+    rate_over_200: 0.15,
+  },
+  {
+    region: 'YT',
+    rate_sub_200: 0.06,
+    rate_over_200: 0.13,
   }
-}
+];
 
-function getTaxCredit(donationAmount: number, highIncomeLimit: boolean, region: string) {
+function getTaxCredit(donationAmount: number, yearlyIncome: number, highIncome: boolean, region: string) {
+  let federalData = canada_charity_tax_credit_data.find(data => data.region === 'CA');
+  let provincialData = canada_charity_tax_credit_data.find(data => data.region === region);
   let federalCredit: number = 0;
   let provincialCredit: number = 0;
-  if (!highIncomeLimit && donationAmount > donation_limit)
+  if (highIncome && donationAmount > donation_limit)
   {
-    federalCredit = (donation_limit *
-                     CanadaCharityTaxCredit.data[data[:][0] == region][1]) +
-                     math.min()
+    federalCredit = (donation_limit * federalData.rate_sub_200) + Math.min(yearlyIncome - high_income_limit, donationAmount - donation_limit) * high_income_tax_credit_rate + ((donationAmount - donation_limit) - Math.min(yearlyIncome - high_income_limit, donationAmount - donation_limit)) * federalData.rate_over_200;
+    provincialCredit = (donation_limit * provincialData.rate_sub_200) + (donationAmount - donation_limit) * provincialData.rate_over_200;
+  }
+  else if (!highIncome && donationAmount > donation_limit)
+  {
+    federalCredit = (donation_limit * federalData.rate_sub_200) + (donationAmount - donation_limit) * federalData.rate_over_200;
+    provincialCredit = (donation_limit * provincialData.rate_sub_200) + (donationAmount - donation_limit) * provincialData.rate_over_200;
   }
   else
   {
-    federalCredit = (donationAmount * canada_charity_tax_credit_data[0][1]);
-    provincialCredit = (donationAmount * canada_charity_tax_credit_data[region][1]);
+    federalCredit = donationAmount * federalData.rate_sub_200;
+    provincialCredit = donationAmount * provincialData.rate_sub_200;
   }
   return [federalCredit, provincialCredit];
 }
 
-const value = getTaxCredit(150, false, 'QC');
+const value = getTaxCredit(20000, 215000, true, 'QC');
 console.log(value[0] + value[1])
